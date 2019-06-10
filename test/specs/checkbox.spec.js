78c0ff30820ff42fc87304e40656a1d3accd995e
邓明 <ming.deng@baifendian.com>
2019-06-10 19:01:44 +0800

    before simulate click

---
+++ test/specs/checkbox.spec.js
@@ -0,0 +1,27 @@
+import Checkbox from "../../src/components/Checkbox";
+
+describe('Checkbox Component', () => {
+  it('set and get component props', () => {
+    const wrapper = shallow(<Checkbox indeterminate={true} checked={false} />);
+
+    const { indeterminate, checked} = wrapper.props();
+    expect(indeterminate).to.equal(true);
+    expect(checked).to.equal(false);
+  });
+
+  it('set and get component props2', () => {
+    const wrapper = shallow(<Checkbox indeterminate={false} checked={false} />);
+
+    const { indeterminate, checked} = wrapper.props();
+    expect(indeterminate).to.equal(false);
+    expect(checked).to.equal(false);
+  });
+
+  it('set and get component props3', () => {
+    const wrapper = shallow(<Checkbox indeterminate={false} checked={true} />);
+
+    const { indeterminate, checked} = wrapper.props();
+    expect(indeterminate).to.equal(false);
+    expect(checked).to.equal(true);
+  });
+});
