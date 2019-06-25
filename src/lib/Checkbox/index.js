fa88e3eee76fd54c807db0d0663fa4f2160da5ca
邓明 <ming.deng@baifendian.com>
2019-06-25 13:23:43 +0800

    remove style

--- src/lib/Checkbox/index.js
+++ src/lib/Checkbox/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Checkbox from 'antd/lib/checkbox';
+// import Checkbox from 'antd/lib/checkbox';
 // import 'antd/lib/checkbox/style/index.less'
 // import 'antd/lib/checkbox/style/css';
 // import DatePicker from 'antd/lib/date-picker'; // 加载 JS
 // import 'antd/lib/date-picker/style/css'
 // import styles from "./style/index.less";
 
-import DatePicker from 'antd/lib/date-picker'; // 加载 JS
-import 'antd/lib/date-picker/style/css';
+// import DatePicker from 'antd/lib/date-picker'; // 加载 JS
+// import 'antd/lib/date-picker/style/css';
 
 const MyCheckbox = props => {
   const { onChange } = props;
-  return <DatePicker></DatePicker>;
+  return null;
   // <Checkbox className={styles.checkbox2} {...props} onChange={(e) => onChange(e.target.checked)} />
 };
 
