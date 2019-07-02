805ccd8d85a4fbbe9826a043ffcd57970b08a19a
邓明 <ming.deng@baifendian.com>
2019-07-02 19:11:15 +0800

    page size options

--- packages/Table/index.js
+++ packages/Table/index.js
@@ -2,56 +2,89 @@ import React from "react";
 import { Table as DefaultTable } from "antd";
 import table from "./table";
 
+import { LocaleProvider } from "antd";
+import zh_CN from "antd/lib/locale-provider/zh_CN";
+
 const { createColumns } = table;
 export default class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
   getInitalColumns(fields) {
     const { extraFields = [] } = this.props;
     return createColumns(fields)
       .enhance(extraFields)
       .values();
   }
-  render() {
+
+  getPagniation() {
     const {
-      fields,
       search = {},
-      datas,
       total = 0,
-      loading = {},
       actions = {},
       onSearch,
       rowKey = "id",
       footer,
       noPage = false,
       pageName = "pageNum",
+      pageSizeOptions,
+      pagination,
       ...others
     } = this.props;
 
-    const columns = this.getInitalColumns(fields);
     const page = search.pageNum ? "pageNum" : pageName;
-    const pagination = noPage
+    const onChange = pn => (onSearch || actions.onSearch)({ [page]: pn });
+
+    let newPagination = noPage
       ? false
       : {
           total,
           current: search[page],
           pageSize: search.pageCount || search.pageSize,
-          onChange: pn => (onSearch || actions.onSearch)({ [page]: pn }),
+          onChange,
           showTotal: t => (footer ? footer({ total, ...search }) : `共 ${t} 条`)
         };
 
+    if (pageSizeOptions && pageSizeOptions.length > 0) {
+      newPagination = {
+        ...newPagination,
+        showSizeChanger: true,
+        onShowSizeChange: onChange,
+        pageSizeOptions
+      };
+    }
+
+    return { ...newPagination, ...pagination };
+  }
+
+  render() {
+    const {
+      fields,
+      datas,
+      loading = {},
+      rowKey = "id",
+      ...others
+    } = this.props;
+
+    const columns = this.getInitalColumns(fields);
+    const newPagination = this.getPagniation();
+
     const tableProps = {
       columns,
-      pagination,
       bordered: true,
       dataSource: datas,
       loading: loading.list,
       rowKey,
-      ...others
+      ...others,
+      pagination: newPagination
     };
 
-    return <DefaultTable {...tableProps} />;
+    return (
+      <LocaleProvider locale={zh_CN}>
+        <DefaultTable {...tableProps} />
+      </LocaleProvider>
+    );
   }
 }
